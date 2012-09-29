77802867a511a3ebc8f87aa9206d8dbc5f774dc8
Troy Parkinson <tparkinson@cox.net>
2012-09-29 13:18:48 -0700

    update to header.php

--- wp-content/themes/simplicity/header.php
+++ wp-content/themes/simplicity/header.php
@@ -7,7 +7,6 @@
   * Description: Handles header and <head> definitions.
   */
 ?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,22 +16,22 @@
 		?>
 	</title>
   <meta charset="<?php bloginfo('charset'); ?>" />
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   
   <!-- CSS & OTHER LINK-RELS -->
-  <link rel="stylesheet" href="" type="text/css" media="all" />
+  <link rel="stylesheet" href="<?php get_template_directory_uri() . '/css/main.css' ?>" type="text/css" media="all" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   
   <!-- JS -->
   <?php 
  	 	wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', '', '1.8.2', false);
-	  wp_enqueue_script('main', get_template_directory() . '/js/main.js', array('jquery'), '1.0', false);
-	  wp_enqueue_script('html5shiv', get_template_directory() . '/js/vendor/html5-3.6-respond-1.1.0.min.js', '', '3.6', false);
-	  wp_enqueue_script('bootstrap', get_template_directory() . '/js/vendor/bootstrap.min', array('jquery'), '1.0', false);
-	  wp_enqueue_script('plugins', get_template_directory() . '/js/plugins.js', array('jquery'), '1.0', false);
+	  wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0', false);
+	  wp_enqueue_script('html5shiv', get_template_directory_uri() . '/js/vendor/html5-3.6-respond-1.1.0.min.js', '', '3.6', false);
+	  wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.min', array('jquery'), '1.0', false);
+	  wp_enqueue_script('plugins', get_template_directory_uri() . '/js/plugins.js', array('jquery'), '1.0', false);
   ?>
   
 </head>
-<body class="<?php body_class(); ?>">
+<body <?php body_class(); ?>>
 	<div id="container">
 		<!-- Topbar -->

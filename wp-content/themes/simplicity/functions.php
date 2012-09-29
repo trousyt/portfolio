9329a656689e4d3f561a3898328900e620249a09
trousyt <tparkinson@cox.net>
2012-09-29 00:15:39 -0700

    addition of new base theme files

---
+++ wp-content/themes/simplicity/functions.php
@@ -0,0 +1,24 @@
+<?php
+ /*
+  * Theme: "Simplicity"
+  * Author: Troy Parkinson
+  * Created: 9/28/2012
+  * File: functions.php
+  * Description: Register this custom theme and define other helper functions.
+  */
+?>
+
+add_action( 'after_setup_theme', 'simplicity_setup' );
+
+if (! function_exists('simplicity_setup')) :
+  function simplicity_setup() {
+    // We are providing our own filter for excerpt_length (or using the unfiltered value)
+    // remove_filter( 'excerpt_length', 'twentyeleven_excerpt_length' );
+    
+    // For language support
+    load_theme_textdomain('simplicity', get_template_directory() . '/languages');
+    
+    // Add support for post formats
+    add_theme_support('post-formats', array('aside', 'link', 'gallery', 'status', 'quote', 'image') );
+  }
+endif;  // /simplicity_setup
---
+++ wp-content/themes/simplicity/index.php
@@ -0,0 +1,14 @@
+<?php
+ /*
+  * Theme: "Simplicity"
+  * Author: Troy Parkinson
+  * Created: 9/28/2012
+  * File: index.php
+  * Description: Handles default first-page structure.
+  */
+?>
+
+<?php get_header(); ?>
+
+
+<?php get_footer(); ?>

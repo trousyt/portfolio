59be1992e7c356e47855f5955d7a0012732f67e3
Troy Parkinson <tparkinson@cox.net>
2012-10-03 23:04:22 -0700

    updated styles and header

--- wp-content/themes/simplicity/header.php
+++ wp-content/themes/simplicity/header.php
@@ -10,23 +10,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>
-		<?php
-			wp_title('|', true, 'right');
-		?>
-	</title>
+  <title><?php wp_title('|', true, 'right'); ?></title>
   <meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   
   <!-- CSS & OTHER LINK-RELS -->
-  <link rel="stylesheet" href="<?php get_template_directory_uri() . '/css/main.css' ?>" type="text/css" media="all" />
+  <?php wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css', false, '1.0', 'all');  ?>
+  <?php wp_enqueue_style('fonts', 'http://fonts.googleapis.com/css?family=Scada|Archivo+Narrow:700,400', false, '1.0', 'all'); ?>
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   
   <!-- JS -->
   <?php 
- 	 	wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', '', '1.8.2', false);
+ 	 	wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js', false, '1.8.2', false);
 	  wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array('jquery'), '1.0', false);
-	  wp_enqueue_script('html5shiv', get_template_directory_uri() . '/js/vendor/html5-3.6-respond-1.1.0.min.js', '', '3.6', false);
+	  wp_enqueue_script('html5shiv', get_template_directory_uri() . '/js/vendor/html5-3.6-respond-1.1.0.min.js', false, '3.6', false);
 	  wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap.min', array('jquery'), '1.0', false);
 	  wp_enqueue_script('plugins', get_template_directory_uri() . '/js/plugins.js', array('jquery'), '1.0', false);
   ?>
@@ -35,3 +32,4 @@
 <body <?php body_class(); ?>>
 	<div id="container">
 		<!-- Topbar -->
+    

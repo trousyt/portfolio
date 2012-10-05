6d696cb316735d579971263e0f77662a52048cd5
Troy Parkinson <tparkinson@cox.net>
2012-10-04 23:20:53 -0700

    changes to functions.php and addition of pages

--- wp-content/themes/simplicity/functions.php
+++ wp-content/themes/simplicity/functions.php
@@ -7,8 +7,13 @@
   * Description: Register this custom theme and define other helper functions.
   */
 
-add_action( 'after_setup_theme', 'simplicity_setup' );
+/* Add standard support to WP */
+add_theme_support('post-thumbnails');
 
+/* SIMPLICITY_SETUP
+ * Setup with custom theme routines.
+ */
+add_action( 'after_setup_theme', 'simplicity_setup' );
 if (! function_exists('simplicity_setup')) :
   function simplicity_setup() {
     // We are providing our own filter for excerpt_length (or using the unfiltered value)
@@ -21,5 +26,84 @@ if (! function_exists('simplicity_setup')) :
     //add_theme_support('post-formats', array('aside', 'link', 'gallery', 'status', 'quote', 'image') );
   }
 endif;  // /simplicity_setup
+
+/* REGISTER_CUSTOM_POST_TYPES
+ * Register custom post types and add any applicable
+ * "taxonomies"/categories.
+ */
+add_action('init', 'register_custom_post_types');
+if (! function_exists('register_custom_post_types')) :
+  function register_custom_post_types() {
+
+    /* These labels show up in the Admin CP */
+    $labels = array(
+      'name' => _x('My Portfolio', 'post type general name'),
+      'singular_name' => _x('Project', 'post type singular name'),
+      'add_new' => _x('Add New', 'portfolio item'),
+      'add_new_item' => __('Add Project'),
+      'edit_item' => __('Edit Project'),
+      'new_item' => __('New Project'),
+      'view_item' => __('View Project'),
+      'search_items' => __('Search Portfolio'),
+      'not_found' => __('Nothing found'),
+      'not_found_in_trash' => __('Nothing found in trash'),
+      'parent_item_colon' => ''
+    );
+
+    $args = array(
+      'labels' => $labels,
+      'public' => true,
+      'publicly_queryable' => true,
+      'show_ui' => true,
+      'query_var' => true,
+      'menu_icon' => get_stylesheet_directory_uri() . 'blah.png',
+      'rewrite' => array('slug' => 'portfolio'),
+      'capability_type' => 'post',
+      'hierarchical' => false,
+      'menu_position' => null,
+      'supports' => array('title','editor','thumbnail')
+    );
+
+    register_post_type('portfolio', args);
+    register_taxonomy(
+      'categories', 
+      array('portfolio'), 
+      array(
+        'hierarchical' => true, 
+        'label' => 'Categories', 
+        'singular_label' => 'Category', 
+        'rewrite' => true
+      )
+    );
+  }
+
+endif;
+
+/* INIT_CUSTOM_ADMIN
+ * Register custom admin UI functionality.
+ */
+add_action('admin_init', 'init_custom_admin')
+if (! function_exists('init_custom_admin')) :
+  function init_custom_admin() {
+    add_meta_box('technologies_used-meta', 'Technologies Used', 'technologies_used', 'portfolio', 'side', 'low');
+  }
+
+  function technologies_used() {
+    global $post;
+    $custom = get_post_custom($post->ID);
+    $technologies_used = $custom['technologies_used'][0];
+    ?>
+    <label>Technologies:</label>
+    <input name="technologies_used" value=<?php echo $technologies_used; ?>>
+    <?php
+  }
+
+  /* Make sure to save the custom post data */
+  add_action('save_post', 'save_custom_post_meta');
+  function save_custom_post_meta() {
+    global $post;
+    update_post_meta($post->ID, 'technologies_used', $_POST['technologies_used']);
+  }
+endif;
 
 ?>
--- wp-content/themes/simplicity/header.php
+++ wp-content/themes/simplicity/header.php
@@ -13,9 +13,15 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <meta name="author" content="Troy Parkinson" />
+  <meta name="contact" content="troy@troypark.com" />
+  <meta name="copyright" content="Copyright (c)2012 Troy Parkinson. All rights reserved." />
+  <meta name="keywords" content="programming, computers, graphic design, ruby, rails, javascript, software, architecture, design" />
+
+  <!-- Add meta data about this specific page and insert into head -->
   
   <!-- CSS & OTHER LINK-RELS -->
-  <?php wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css', false, '1.0', 'all');  ?>
+  <?php wp_enqueue_style('main', get_stylesheet_directory_uri() . '/main.css', false, '1.0', 'all');  ?>
   <?php wp_enqueue_style('fonts', 'http://fonts.googleapis.com/css?family=Scada|Archivo+Narrow:700,400', false, '1.0', 'all'); ?>
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
   

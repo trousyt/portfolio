09d2c8acb71916783a765cd7175cc5e4da70565a
Troy Parkinson <tparkinson@cox.net>
2012-10-11 22:13:58 -0700

    minor style changes

--- wp-content/themes/simplicity/index.php
+++ wp-content/themes/simplicity/index.php
@@ -12,7 +12,7 @@
 
 <!-- #header -->
 <div id="index-header" class="row">
-	<div class="span10">
+	<div class="span10 offset 2">
 		<!-- Logo Here -->
 		Logo Placeholder
 	</div>
@@ -21,50 +21,51 @@
 
 <!-- /#header -->
 
-<!-- #hero -->
-<div id="hero" class="row">
-	<div class="span10">
+<div class="span10 offset2">
+	<!-- #hero -->
+	<div id="hero" class="row">
 		<h1><span class="large">Hi,</span> my name is <strong>Troy Parkinson</strong> and I'm a software engineer, graphic designer, and gosh-darnit, good guy that loves to create <em>practical</em> web apps, iOS apps, and other intuitive software that works using standards-driven design.</h1>
 		<div class="row">
 			<button class="btn btn-primary btn-large btn-hero centered-block">Contact me today to <strong>have a chat</strong>.</button>
 		</div>
 	</div>
-</div>
-<!-- /#hero -->
+	<!-- /#hero -->
 
-<!-- #body-content -->
-<div id="body-content" class="row">
-	<div class="content-header span10 row">
-		<div id="slider" class="span4">
-			<!-- Slider code here -->Some placeholder text
+	<!-- #body-content -->
+	<div id="body-content" class="row">
+		<div class="content-header row">
+			<div id="slider" class="span4">
+				<!-- Slider code here -->Some placeholder text
+			</div>
+			<div id="body-nav" class="span4">
+				<div class="arrow"></div>
+				<ul class="unstyled">
+					<li class="contact-button"><a href="#" title="Contact">Contact</a></li>
+					<li class="about-button"><a href="#" title="About Me">About</a></li>
+					<li class="work-button"><a href="#" title="Work">Work</a></li>
+					<li class="blog-button"><a href="#" title="Blog">Blog</a></li>
+				</ul>
+			</div>
 		</div>
-		<div id="body-nav" class="span4">
-			<div class="arrow"></div>
-			<ul class="unstyled">
-				<li class="contact-button"><a href="#" title="Contact">Contact</a></li>
-				<li class="about-button"><a href="#" title="About Me">About</a></li>
-				<li class="work-button"><a href="#" title="Work">Work</a></li>
-				<li class="blog-button"><a href="#" title="Blog">Blog</a></li>
-			</ul>
-		</div>
-	</div>
 
-	<!-- #contact-me -->
-	<div id="contact-me" class="row">
-		<div class="span5 offset3 rotate-neg5 ">
-			<form id="contact-form">
-				<div class="row">
-					<input id="name" type="text" placeholder="Name" />
-					<input id="email" type="email" placeholder="Email" />
-				</div>
-				<div class="row">
-					<h3>Let's chat about...</h3>
-					<textarea rows="5"></textarea>
-				</div>
-			</form>
+		<!-- #contact-me -->
+		<div id="contact-me" class="row">
+			<div class="span5 offset3 rotate-neg5 ">
+				<form id="contact-form">
+					<div class="row">
+						<input id="name" type="text" placeholder="Name" />
+						<input id="email" type="email" placeholder="Email" />
+					</div>
+					<div class="row">
+						<h3>Let's chat about...</h3>
+						<textarea rows="5"></textarea>
+					</div>
+				</form>
+			</div>
 		</div>
+		<!-- /#contact-me -->
 	</div>
-	<!-- /#contact-me -->
+	<!-- /span10 offset2 -->
 </div>
 <!-- /#body-content -->
 

430dc7a9c0618e362a18e27c3674083c4737ccb5
Troy Parkinson <tparkinson@cox.net>
2012-11-10 22:08:21 -0800

    testing

--- wp-content/themes/simplicity/functions.php
+++ wp-content/themes/simplicity/functions.php
@@ -4,9 +4,9 @@
   * Description: Require only necessary files here.
   */
 
-require_once locate_template('/lib/config.php');
-require_once locate_template('/lib/enqueue.php');
 require_once locate_template('/lib/register.php');
-require_once locate_template('/lib/custom.php');
+require_once locate_template('/lib/config.php');
+/*require_once locate_template('/lib/enqueue.php');
+require_once locate_template('/lib/custom.php');*/
 
  ?>

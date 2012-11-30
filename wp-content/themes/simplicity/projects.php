f5bca61f97d0184b2be0a329a31e6202155a387d
Troy Parkinson <tparkinson@cox.net>
2012-11-29 21:52:27 -0800

    split template

--- wp-content/themes/simplicity/projects.php
+++ wp-content/themes/simplicity/projects.php
@@ -8,70 +8,9 @@
   */
 ?>
 
-<?php get_header(); ?>
 
-          <!-- section#content-wrapper -->
-          <section id="work">
+<?php get_template_part('templates/projects', 'header') ?>
 
-            <!-- header -->
-            <header class="buffer ">
-              <div class="row">
-                <div class="span4"> <!-- offset 2 -->
-                  <div class="logo">
-                    <a href="index.html">Placeholder</a>
-                  </div>
-                </div>
-                <div class="span6">
-                  <div class="nav pull-right">
-                    <ul>
-                      <li class="contact nav-item">
-                        <a href="contact.html"><div class="bl contact-back"></div>Contact</a>
-                      </li>
-                      <li class="about nav-item">
-                        <a href="about.html"><div class="bl about-back"></div>About</a>
-                      </li>
-                      <li class="work nav-item active">
-                        <a href="work.html"><div class="bl work-back"></div>Work</a>
-                      </li>
-                      <li class="blog nav-item">
-                        <a href="blog.html"><div class="bl blog-back"></div>Blog</a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="divider"></div>
-              </div>
-            </header>
-            
-            <!-- #body-wrapper -->
-            <div id="body-wrapper" class="buffer2x">
-              <!-- section#body -->
-              <section id="body">
-
-              	<?php 
-              	// Start the page loop.
-              	while ( have_posts() ) : the_post();
-              		$custom_fields = get_post_custom();
-              	?>
-
-                <header>
-                  <div class="row">
-                    <div class="span7">
-                      <h3 class="tagline em">Portfolio Schwartfolio.</h3>
-                    </div>
-                    <div class="span3 right-align">
-                      <h1>Work</h1>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="divider1"></div>
-                  </div>
-                </header>
-                
-                <!-- .content -->
-                <div class="content buffer">
                   <?php the_content() ?>
 
                   <!-- #portfolio -->
@@ -97,16 +36,4 @@
                   </div>
                   <!-- /#portfolio -->
                   
-                </div>
-                <!-- /.content -->
-                
-                <?php endwhile; ?>
-
-              </section>
-              <!-- /section#body -->
-            </div>
-            <!-- /#body-wrapper -->
-          </section>
-          <!-- /section#content-->
-      
-<?php get_footer(); ?>
+<?php get_template_part('templates/projects', 'footer') ?>
---
+++ wp-content/themes/simplicity/templates/projects-footer.php
@@ -0,0 +1,13 @@
+				</div>
+                <!-- /.content -->
+                
+                <?php endwhile; ?>
+
+              </section>
+              <!-- /section#body -->
+            </div>
+            <!-- /#body-wrapper -->
+          </section>
+          <!-- /section#content-->
+      
+<?php get_footer(); ?>

1370c8364f108b0e178e2b07235fd27ad53a5440
Troy Parkinson <tparkinson@cox.net>
2012-11-29 21:00:17 -0800

    more portfolio changes

--- wp-content/themes/simplicity/projects.php
+++ wp-content/themes/simplicity/projects.php
@@ -84,13 +84,7 @@
 										?>
 										
 										<div class="portfolio-item">
-                      <div class="img" alt="<?php the_content() ?>"
-                      	<?php 
-                      	if (has_post_thumbnail()) {
-                      		echo ' style="background-image: url(\'<?php the_post_thumbnail() ?>\');"'
-                      	} 
-                      	?>>
-                      </div>
+                      <div class="img" alt="<?php the_content() ?>" style="background-image: url('<?php the_post_thumbnail() ?>');"></div>
                       <aside><div><a href="<?php the_permalink() ?>"><?php the_title() ?></a> / <?php the_category(', ') ?></div><div><small>Tags</small></div></aside>
                     </div>
 										

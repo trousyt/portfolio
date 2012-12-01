46f0128b4854ef34a8cc528a92c443a2f7b68075
Troy Parkinson <tparkinson@cox.net>
2012-12-01 14:32:19 -0800

    small stylistic changes

--- wp-content/themes/simplicity/footer.php
+++ wp-content/themes/simplicity/footer.php
@@ -10,8 +10,8 @@
     <!-- .footer -->
     <footer class="footer center-align">
       <div class="container">
-        <div class="row">
-          <div class="span10">
+        <div class="span10">
+          <div class="row">
             <ul class="unstyled">
               <li><a href="/contact" title="Contact">Contact</a></li>
               <li><a href="/about" title="About Me">About</a></li>
@@ -19,9 +19,9 @@
               <li><a href="/blog" title="Blog">Blog</a></li>
             </ul>
           </div>
-        </div>
-        <div class="row">
-          <strong>Contact Me</strong> @ <a href="mailto:troy@troypark.com">troy@troypark.com</a> or <a href="tel:17022859127">+1 702 285-9127</a>
+          <div class="row">
+            <strong>Contact Me</strong> @ <a href="mailto:troy@troypark.com">troy@troypark.com</a> or <a href="tel:17022859127">+1 702 285-9127</a>
+          </div>
         </div>
       </div>
     </footer>

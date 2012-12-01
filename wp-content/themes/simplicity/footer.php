137df081b7f211311d982529110dce4ec2acdba4
Troy Parkinson <tparkinson@cox.net>
2012-12-01 14:33:46 -0800

    small stylistic changes

--- wp-content/themes/simplicity/footer.php
+++ wp-content/themes/simplicity/footer.php
@@ -11,16 +11,18 @@
     <footer class="footer center-align">
       <div class="container">
         <div class="span10">
-          <div class="row">
-            <ul class="unstyled">
-              <li><a href="/contact" title="Contact">Contact</a></li>
-              <li><a href="/about" title="About Me">About</a></li>
-              <li><a href="/portfolio" title="Portfolio">Portfolio</a></li>
-              <li><a href="/blog" title="Blog">Blog</a></li>
-            </ul>
-          </div>
-          <div class="row">
-            <strong>Contact Me</strong> @ <a href="mailto:troy@troypark.com">troy@troypark.com</a> or <a href="tel:17022859127">+1 702 285-9127</a>
+          <div class="wrapper">
+            <div class="row">
+              <ul class="unstyled">
+                <li><a href="/contact" title="Contact">Contact</a></li>
+                <li><a href="/about" title="About Me">About</a></li>
+                <li><a href="/portfolio" title="Portfolio">Portfolio</a></li>
+                <li><a href="/blog" title="Blog">Blog</a></li>
+              </ul>
+            </div>
+            <div class="row">
+              <strong>Contact Me</strong> @ <a href="mailto:troy@troypark.com">troy@troypark.com</a> or <a href="tel:17022859127">+1 702 285-9127</a>
+            </div>
           </div>
         </div>
       </div>

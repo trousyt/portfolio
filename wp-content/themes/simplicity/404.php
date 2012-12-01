4ba7b96c760a77450800b57e7b93bf9443573dc4
Troy Parkinson <tparkinson@cox.net>
2012-11-30 23:38:07 -0800

    404 page changes

--- wp-content/themes/simplicity/404.php
+++ wp-content/themes/simplicity/404.php
@@ -90,10 +90,10 @@
 						  }
 						  echo " " . $website . $_SERVER['REQUEST_URI']; 
 						?> 
-						and it doesn't exist. Hmm... Perhaps a Tardis would come in handy. <?php echo $casemessage; ?>  Anyhow, we <em>profusely</em> apologize for the error! May we suggest using the above navigation or use the back button in your browser to get out of here?
+						and it doesn't exist. Hmm... Perhaps a Tardis would come in handy. <?php echo $casemessage; ?>  Anyhow, we <em>profusely</em> apologize for the error! May we suggest using the above navigation or the back button in your browser to get out of here?
 					</p>
 					<p class="center-align">
-						<img src="<?php get_template_directory_uri() . '/img/layout_404.png' ?>" title="Page not found.">
+						<img src="<?php bloginfo( 'template_directory' ) . '/img/layout_404.png' ?>" title="Page not found.">
 					</p>
 
                 </div>
